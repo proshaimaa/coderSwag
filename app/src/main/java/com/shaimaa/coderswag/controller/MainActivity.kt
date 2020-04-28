6e8ab3f4b66f0adf10f823687a3a4827460228e8
Shaimaa Elshafei <pro.shaimaa@gmail.com>
2020-04-28 22:56:09 +0300

    list view

--- app/src/main/java/com/shaimaa/coderswag/controller/MainActivity.kt
+++ app/src/main/java/com/shaimaa/coderswag/controller/MainActivity.kt
@@ -3,12 +3,21 @@ package com.shaimaa.coderswag.controller
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.shaimaa.coderswag.R
+import com.shaimaa.coderswag.adapters.CategoryAdapter
+import com.shaimaa.coderswag.services.DataService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var adapter : CategoryAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        adapter = CategoryAdapter(this,DataService.categories)
+
+        categoryListView.adapter = adapter
 
     }
 }
